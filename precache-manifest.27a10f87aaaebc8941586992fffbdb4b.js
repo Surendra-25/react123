9911d2cb4e59c4eb2512ed7325bbce61bf8093e5
surendra-25 <surendra892525@gmail.com>
2020-08-05 16:25:08 +0530

    Updates

---
+++ precache-manifest.27a10f87aaaebc8941586992fffbdb4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8956b7c02dda3e951a39dd9a6fd35ab2",
+    "url": "/Surendra-25/react123/index.html"
+  },
+  {
+    "revision": "2887d4b21ab04d2805f3",
+    "url": "/Surendra-25/react123/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/Surendra-25/react123/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Surendra-25/react123/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2887d4b21ab04d2805f3",
+    "url": "/Surendra-25/react123/static/js/main.dd925428.chunk.js"
+  },
+  {
+    "revision": "41b7408d3df695e53e71",
+    "url": "/Surendra-25/react123/static/js/runtime-main.eb560b1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Surendra-25/react123/static/media/logo.5d5d9eef.svg"
+  }
+]);
